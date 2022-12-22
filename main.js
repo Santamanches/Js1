aa6623fdf60d7c0ca90f1d354752877d48713529
Santamanches <santamanches@gmail.com>
2022-12-22 19:11:10 +0300

    Save

---
+++ main.js
@@ -0,0 +1,85 @@
+// console.log(10); - это способ вывода информации в консоль браузера
+
+//console.log('Zafar'); строковый тип данных
+//console.log(20);  числовой тип данных
+//console.log(true);  логический тип данных true
+//console.log(false); логический тип данных false
+
+
+// var imya = 'Zafar';
+// var age = 100;
+// imya = 'JS';
+
+// +  специальный оператор склеивания (конкатенация)
+
+// console.log('Я ' + imya + ' мне ' + age + ' лет' );
+
+// var number = 20
+// var myWord = 'Мое число ' + number
+
+// console.log(myWord);
+
+// prompt() - создает нам окошко для ввода данных
+
+// + перед функцией или переменной преобразует его в числовой тип данных
+
+// alert() - создает нам модальное окно
+
+
+// var str = +prompt('Введите любое слово');
+
+// alert(str);
+
+// console.log('10 + 5 = ' + (10 + 5) );
+// console.log('10 - 5 = ' + (10 - 5) );
+// console.log('10 * 5 = ' + 10 * 5 );
+// console.log('10 / 5 = ' + 10 / 5 );
+
+// console.log('10 % 3 = ' + 12 % 5);
+
+
+
+
+var str = prompt('Введи свое имя:')
+
+console.log(' Вот ваши ответы ' + (str) + ': ');
+
+alert('Спасибо ' + str + ' а теперь пойдем решать примеры...')
+
+
+
+var chislo = (1000-7)
+
+var str = +prompt('1000-7 ?')
+
+console.log(' 1000-7=' + chislo + ' , Ваш ответ : ' + str);
+
+
+var chislo = (225+775)
+
+var str = +prompt('225+775')
+
+console.log(' 225+775=' + chislo + ' , Ваш ответ : ' + str);
+
+
+var chislo = (25*4)
+
+var str = +prompt('25*4 ?')
+
+console.log(' 25*4=' + chislo + ' , Ваш ответ : ' + str);
+
+var chislo = (56/7)
+
+var str = +prompt('56/7 ?')
+
+console.log(' 56/7=' + chislo + ' , Ваш ответ : ' + str);
+
+var chislo = (12%5)
+
+var str = +prompt('12 % 5 ?')
+
+console.log(' 12 % 5=' + chislo + ' , Ваш ответ : ' + str);
+
+
+
+alert('Мегахарош если решил всё, а теперь проверь свои ответы в консоли')
